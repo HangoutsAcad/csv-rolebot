3c7d79435364b9e3cbe3f6d4ca58cea1618527ba
ash <ash@schnoor.email>
2022-12-15 13:08:16 -0500

    modify status

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,5 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-    setInterval(() => client.user.setActivity({ name: 'meow', type: ActivityType.Streaming }), 22000);
+    setInterval(() => client.user.setActivity({ name: 'for meowers', type: ActivityType.Listening }), 22000);
 }};
