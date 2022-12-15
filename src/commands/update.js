10df1906ad30aae83d0d3e712d25ab941e4002de
ash <ash@schnoor.email>
2022-12-15 18:40:29 -0500

    update command

--- src/commands/update.js
+++ src/commands/update.js
@@ -3,18 +3,20 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const fs = require('fs');
 const path = require('path');
 const csv = require('fast-csv');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("update")
         .setDescription("Update the server's roles."),
-    // komuta seçenekler eklemek istersen guide: https://discordjs.guide/interactions/slash-commands.html#options
+    // https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
+        await interaction.deferReply();
         fs.createReadStream(path.resolve('./', 'Hangouts.csv'))
             .pipe(csv.parse({ headers: true }))
             .on('error', error => console.error(error))
-            .on('data', row => console.log(row))
-            .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
-        interaction.reply(`Done`)
+            .on('error', error => interaction.reply(`${error}`))
+            //.on('data', row => console.log(row))
+            .on('end', rowCount => interaction.reply(`Parsed ${rowCount} rows`));
     }
 };
