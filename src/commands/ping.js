d30c5872e73e463d4f66347eb3991ae4141120f5
ash <ash@schnoor.email>
2022-12-15 13:08:11 -0500

    update desc

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -4,7 +4,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ping")
-    .setDescription("Pong!"),
+    .setDescription("View the ping of the bot and Discord API."),
     // komuta seçenekler eklemek istersen guide: https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
       interaction.reply(`🏓 Latency is **${Date.now() - interaction.createdTimestamp}**ms. API Latency is **${Math.round(client.ws.ping)}**ms`)
