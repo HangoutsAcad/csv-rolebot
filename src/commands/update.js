6c64ca6f59032eab9979d324a0b749631324131b
ash <ash@schnoor.email>
2022-12-16 09:47:51 -0500

    hmmmmmmmm

--- src/commands/update.js
+++ src/commands/update.js
@@ -4,12 +4,30 @@ const fs = require('fs');
 const path = require('path');
 const wait = require('node:timers/promises').setTimeout;
 
+const csvFilePath=('./Hangouts.csv')
+const csv=require('csvtojson')
+const converter=csv({
+    noheader:false,
+    ignoreColumns:/(Team|Individual|Calculator)/,
+})
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("update")
         .setDescription("Update the server's roles."),
     // https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
+        csv()
+            .fromFile(csvFilePath)
+            .then((jsonObj)=>{
+                console.log(jsonObj);
+                interaction.reply(`Converted`)
 
+            })
+            .on('error',(err)=>{
+                console.log(err)
+                interaction.reply(err)
+            })
+        const jsonArray=await csv().fromFile(csvFilePath);
     }
 };
