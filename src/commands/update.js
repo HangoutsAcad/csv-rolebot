a8973c6dd119b12eb327dec61bd3489aa0d4f952
ash <ash@schnoor.email>
2022-12-16 09:32:06 -0500

    f fast-csv

--- src/commands/update.js
+++ src/commands/update.js
@@ -2,10 +2,7 @@ const { EmbedBuilder, PermissionsBitField } = require("discord.js");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const fs = require('fs');
 const path = require('path');
-const csv = require('fast-csv');
 const wait = require('node:timers/promises').setTimeout;
-
-const stream = fs.createReadStream('./Hangouts.csv');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -13,10 +10,6 @@ module.exports = {
         .setDescription("Update the server's roles."),
     // https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
-        csv.parseStream(stream)
-            .on('error', error => console.error(error))
-            .on('data', row => console.log(`ROW=${JSON.stringify(row)}`))
-            .on('end', rowCount => interaction.reply(`Parsed ${rowCount} rows`) & console.log(`Parsed ${rowCount} rows`));
 
     }
 };
