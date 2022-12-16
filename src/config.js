601a500a20fe683f2d9acbd464cf852f5fdc2a53
ash <ash@schnoor.email>
2022-12-16 10:16:40 -0500

    idea gitignore

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   prefix: "/",
   owner: "690639157013381220",
-  token: "MTA1MTg1NTM3MTA3MDg2NTQ5OQ.GX2HWb.9YydCwusBw6l2noDZ35ZSQLgeQGWwHnT8zXcb8",
+  token: "MTA1MTg1NTM3MTA3MDg2NTQ5OQ.G6ENDA.BUVFM-f7j2DZx2nhWzsc8ibYhlvLFzA_htPRn8",
   clientid: "1051855371070865499",
   guildid: "1051855040295481444"
 }
