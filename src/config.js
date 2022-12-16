ebe91015e1c387d45d67043728344f7f1fea32b1
ash <ash@schnoor.email>
2022-12-16 10:18:00 -0500

    delete config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  prefix: "/",
-  owner: "690639157013381220",
-  token: "MTA1MTg1NTM3MTA3MDg2NTQ5OQ.G6ENDA.BUVFM-f7j2DZx2nhWzsc8ibYhlvLFzA_htPRn8",
-  clientid: "1051855371070865499",
-  guildid: "1051855040295481444"
-}
