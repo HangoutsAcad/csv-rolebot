d1c6e36904076a01c5229d72ca10d0a6e584af6a
ash <ash@schnoor.email>
2022-12-16 09:28:39 -0500

    messing with update

--- src/commands/update.js
+++ src/commands/update.js
@@ -5,18 +5,18 @@ const path = require('path');
 const csv = require('fast-csv');
 const wait = require('node:timers/promises').setTimeout;
 
+const stream = fs.createReadStream('./Hangouts.csv');
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("update")
         .setDescription("Update the server's roles."),
     // https://discordjs.guide/interactions/slash-commands.html#options
-    //run: async (client, interaction) => {
-        //await interaction.deferReply();
-        //fs.createReadStream(path.resolve('./', 'Hangouts.csv'))
-            //.pipe(csv.parse({ headers: true }))
-            //.on('error', error => console.error(error))
-            //.on('error', error => interaction.reply(`${error}`))
-            //.on('data', row => console.log(row))
-            //on('end', rowCount => interaction.reply(`Parsed ${rowCount} rows`));
-    //}
+    run: async (client, interaction) => {
+        csv.parseStream(stream)
+            .on('error', error => console.error(error))
+            .on('data', row => console.log(`ROW=${JSON.stringify(row)}`))
+            .on('end', rowCount => interaction.reply(`Parsed ${rowCount} rows`) & console.log(`Parsed ${rowCount} rows`));
+
+    }
 };
