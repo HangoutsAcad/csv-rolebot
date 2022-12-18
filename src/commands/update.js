6e66fb88f77ea0c66e779a12425743195a26534f
ash <ash@schnoor.email>
2022-12-18 02:34:04 -0500

    FOUND THE DATA ENTRY BUT IT SAYS HE DOESN'T EXIST DEAR LORD HELP ME

--- src/commands/update.js
+++ src/commands/update.js
@@ -15,19 +15,23 @@ module.exports = {
     // https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
         const guild = client.guilds.cache.get(guildId);
-        csv({
-            ignoreColumns:/(Team|Individual|Calculator)/
-        }).fromFile(csvFilePath).then(rows => {
-            rows.forEach(row => {
-                const member = guild.members.cache.find(m => m.user.username === row.Discord);
-                console.log(`Found ${member}`)
+        csv().fromFile('./Takumi.csv').then(jsonObj => {
+            // Iterate through each row in the JSON object
+            jsonObj.forEach(row => {
+                // Get the Discord user and role from the row
+                const discordUser = row['Discord'];
+                const role = row['Rank'];
+
+                // Get the actual Discord user object by their username
+                const member = guild.members.cache.find(m => m.user.username === discordUser);
+
+                // Check if the user was found
                 if (member) {
-                    const role = guild.roles.cache.find(r => r.name === row.Rank);
-                    console.log(`Found ${role}`)
-                    if (role) {
-                        member.roles.set([role]).catch(console.error);
-                        console.log(`Appled ${role} to ${member}`)
-                    }
+                    // Add the role to the user
+                    member.roles.add(role);
+                    console.log(`Assigned role ${role} to ${discordUser}`);
+                } else {
+                    console.log(`Could not find user ${discordUser}`);
                 }
             });
         });
