9a8d5419f8d996f1f755121099ae3d7f6c73d200
ash <ash@schnoor.email>
2022-12-18 13:17:42 -0500

    finally got it to fucking work

--- src/commands/update.js
+++ src/commands/update.js
@@ -5,7 +5,7 @@ var colors = require('colors');
 const pink = colors.magenta
 const log = console.log;
 
-const csvFilePath=('./Takumi.csv')
+const csvFilePath=('./TEST.csv')
 const csv=require('csvtojson')
 const config = require("../config");
 const guildId = config.guildid
@@ -18,24 +18,30 @@ module.exports = {
     // https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
         const guild = client.guilds.cache.get(guildId);
+        interaction.reply(`Found CSV...loading now...`)
+        const message = await interaction.fetchReply();
         csv().fromFile(csvFilePath).then(jsonObj => {
+
             //console.log(jsonObj)
             // Iterate through each row in the JSON object
             jsonObj.forEach(row => {
                 // Get the Discord user and role from the row
                 const discordUser = row['Discord'];
                 const role = row['Rank ID'];
+                const pretty = row['Rank'];
 
                 // Get the actual Discord user object by their username
                 const member = guild.members.cache.find(m => m.user.tag === discordUser);
+
 
                 // Check if the user was found
                 if (member) {
                     // Add the role to the user
                     member.roles.add(role);
-                    log(`Assigned role` + pink(` ${role}`) + 'to' + pink(` ${discordUser}/${member}`));
+                    log(`Assigned role` + pink(` ${pretty} (${role})`) + ' to' + pink(` ${discordUser}/${member}`));
+                    message.channel.send(`Assigned ${pretty} to ${discordUser}`)
                 } else {
-                    log(`Could not assign` + pink(` ${role}`) + `to` + pink(` ${discordUser}/${member}`));
+                    //log(`Could not assign` + pink(` ${pretty} (${role})`) + ` to` + pink(` ${discordUser}/${member}`));
                 }
             });
         });
