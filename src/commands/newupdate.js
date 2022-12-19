9cac96abae402e535ca76765af9641be1ca250e8
ash <ash@schnoor.email>
2022-12-19 13:25:44 -0500

    im slow

--- src/commands/newupdate.js
+++ src/commands/newupdate.js
@@ -22,7 +22,32 @@ module.exports = {
     // https://discordjs.guide/interactions/slash-commands.html#options
     run: async (client, interaction) => {
 
-        }
+        csv()
+            .fromFile(csvFilePath)
+            .then(jsonObj => {
+                // Next, we need to get a list of all the members of the server
+                interaction.guild.members.fetch().then(members => {
+                    // Now we can iterate through each member and see if they are in the CSV file
+                    members.forEach(member => {
+                        // Check if the member's username is in the CSV file
+                        const csvMember = jsonObj.find(m => m.username === member.user.username);
+                        if (csvMember) {
+                            // If the member is in the CSV file, check if they have the correct role
+                            const roleName = csvMember.role;
+                            const role = interaction.guild.roles.cache.find(r => r.name === roleName);
+                            if (!member.roles.cache.has(role.id)) {
+                                // If the member does not have the correct role, remove all of their current roles
+                                // except for the one that is specified in the CSV file
+                                const rolesToRemove = member.roles.cache.filter(r => r.name !== roleName);
+                                member.roles.remove(rolesToRemove).catch(console.error);
+                                // Now add the correct role to the member
+                                member.roles.add(role).catch(console.error);
+                            }
+                        }
+                    });
+                });
+            })
+    }
 }
 
 //log(`Assigned role` + pink(` ${pretty} (${role})`) + ' to' + pink(` ${discordUser}/${member}`));
